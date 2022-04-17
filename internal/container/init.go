a430e4c84aa76a985a08ca1e480cfce5d7f6bc5b
Elias.Yao <siran0611@gmail.com>
2022-04-17 23:46:36 +0800

    use aufs wrapper busybox.

--- internal/container/init.go
+++ internal/container/init.go
@@ -68,26 +68,51 @@ func setUpMount() {
 
 	// mount proc
 	defaultMointFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMointFlags), "")
+	err = syscall.Mount("proc", "/proc", "proc", uintptr(defaultMointFlags), "")
+	if err != nil {
+		logrus.Errorf("mount proc error; %v", err)
+		return
+	}
 
-	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+	err = syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+	if err != nil {
+		logrus.Errorf("mount tmpfs error; %v", err)
+		return
+	}
 }
 
 func pivotRoot(root string) error {
-	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
-		return errors.New(fmt.Sprintf("mount rootfs to iteself error: %v", err))
+	// systemd 加入linux 之后， mount namespace 就编程 shared by default, 必须显示声明新的 mount namespac 独立
+	err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	if err != nil {
+		return errors.New(fmt.Sprintf("mount systemd error; %v", err))
 	}
+
+	// 重新mount root
+	// bind mount 将相同内容换挂载点
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return errors.New(fmt.Sprintf("mount rootfs to itself error: %v", err))
+	}
+
+	// 创建 rootfs/.pivot_root 存储旧的 old_root
 	pivotDir := filepath.Join(root, ".pivot_root")
-	if err := os.Mkdir(pivotDir, 07777); err != nil {
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
 		return err
 	}
+
+	// pivot_root 到新的rootfs, 老的 old_root挂载在rootfs/.pivot_root
+	// 挂载点现在依然可以在mount命令中看到
 	if err := syscall.PivotRoot(root, pivotDir); err != nil {
 		return errors.New(fmt.Sprintf("pivot_root error; %v", err))
 	}
+
+	// 修改当前的工作目录到根目录
 	if err := syscall.Chdir("/"); err != nil {
 		return errors.New(fmt.Sprintf("chdir / error;%v", err))
 	}
+
 	pivotDir = filepath.Join("/", ".pivot_root")
+
 	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
 		return errors.New(fmt.Sprintf("unmount pivot_root dir error; %v", err))
 	}
